fe33344ab124c011ff47b6ec2814b6d1d8962667
Peter Tardif <peter.tardif@gmail.com>
2019-03-28 23:01:46 -0600

    cat carousel finished

--- catCarousel/index.js
+++ catCarousel/index.js
@@ -1,10 +1,10 @@
 $(function() {
     $('.thumbnails').on('click','img',function(event){
         // capture the click on the img and id the image
-        let selectedImage = event.target;
+        // let selectedImage = event.target;
         // select the div to hold and copy the img to .hero class
-        $('.hero').html(selectedImage);
-
+        $('.hero img').remove();
+        $(event.target).clone().appendTo( '.hero' );
     })
   }
 );
